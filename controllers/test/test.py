49e10f8529b9eff028e4f3f4f0c6ba7d20d0b871
Wojtazzs <60355100+Wojtazzs@users.noreply.github.com>
2024-06-22 19:03:46 +0200

    Added position sensor and simple logic for it

--- controllers/test/test.py
+++ controllers/test/test.py
@@ -6,13 +6,12 @@ from controller import Robot, Motor
 from math import pi, sin
 from enum import Enum
 
-TIME_STEP = 64
+TIME_STEP = 16
 robot = Robot()
 kij_motor = robot.getDevice("kij_motor")
-#kij_motor.setAcceleration(10)
-kij_motor.setTorque(3000)
-kij_motor.setPosition(float('inf'))
-kij_motor.setVelocity(0.0)
+kij_position = robot.getDevice("kij_position")
+
+kij_position.enable(1000)
 
 class Angle():
     val = 0
@@ -24,9 +23,21 @@ class Angle():
 F = 2.0   # frequency 2 Hz
 t = 0.0   # elapsed simulation time
 angle = Angle()
+
+to_pos = 3.14
+starting_pos = False
 while robot.step(TIME_STEP) != -1:
-    kij_motor.setVelocity(10.0)
+    pos = kij_position.getValue()
     
+    if starting_pos == False:
+        kij_motor.setPosition(-1)
+        if pos <= 0.0:
+            starting_pos = True
+        continue
+            
+    if pos >= 3.13 or pos <= -3.13:
+        to_pos = -to_pos
+    kij_motor.setPosition(to_pos)   
     #if kij_motor.
     #position = angle.set_val(t)
     #print(position)
