0cd5d415bf72e58b1de7f9173b675f51a014ad98
Wojtazzs <60355100+Wojtazzs@users.noreply.github.com>
2024-06-23 21:52:13 +0200

    Added rotation and scoring for both holes

--- controllers/test/test.py
+++ controllers/test/test.py
@@ -10,8 +10,11 @@ TIME_STEP = 16
 robot = Robot()
 kij_motor = robot.getDevice("kij_motor")
 kij_position = robot.getDevice("kij_position")
+kij_brake = robot.getDevice("kij_brake")
+rotation = robot.getDevice("rotational motor")
 
 kij_position.enable(1000)
+kij_brake.setDampingConstant(0)
 
 class Angle():
     val = 0
@@ -26,8 +29,34 @@ angle = Angle()
 
 to_pos = 3.14
 starting_pos = False
+returning = False
+
+
+class History():
+    _number = 0
+    array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    def avg(self):
+        sum = 0
+        i = 0
+        while i < len(self.array):
+            sum += self.array[i]
+            i += 1
+        return sum/len(self.array)
+        
+    def new(self, val):
+        self.array[self._number] = val
+        self._number += 1
+        if self._number == len(self.array):
+            self._number = 0
+
+history = History()
+
 while robot.step(TIME_STEP) != -1:
     pos = kij_position.getValue()
+    
+    history.new(pos)
+    print(to_pos)
     
     if starting_pos == False:
         kij_motor.setPosition(-1)
@@ -35,14 +64,19 @@ while robot.step(TIME_STEP) != -1:
             starting_pos = True
         continue
             
-    if pos >= 3.13 or pos <= -3.13:
-        to_pos = 0
-    kij_motor.setPosition(to_pos)   
-    #if kij_motor.
-    #position = angle.set_val(t)
-    #print(position)
-    #kij_motor.setPosition(position)
-    #kij_motor.setVelocity(10.0)
-    #t += TIME_STEP / 100.0
+    if pos >= 1.1 or pos <= -1.1:
+        returning = True
+        kij_brake.setDampingConstant(5000)
 
+    if returning:
+        rotation.setPosition(pi/2)
+        returning = False
+        to_pos = 0
+    
+    if returning == False and 0.01 >= history.avg() >= -0.01:
+        starting_pos = False
+        rotation.setPosition(0)
+        kij_brake.setDampingConstant(0)
+    
+    kij_motor.setPosition(to_pos)
     pass
